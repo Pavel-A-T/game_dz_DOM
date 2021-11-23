edfe8f8eaa02729550cdba6da538b3a31eba55a3
Pavel Titov <otrepiev.grishka@yandex.ru>
2021-11-23 19:31:38 +0300

    добавил условие дважды подряд нельзя в одну ячейку

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,14 @@
 export default class App {
+  constructor() {
+    this.calculateRandom = () => Math.floor(Math.random() * 15);
+  }
+
   getRandomGoblin() {
     if (!this.goblin) this.goblin = document.getElementById('goblin');
     if (!this.box) this.box = document.getElementsByClassName('box');
-    const random = Math.floor(Math.random() * 15);
+    this.random = this.getRandom();
     for (const element of this.box) {
-      if (element.getAttribute('data-id') === random.toString()) {
+      if (element.getAttribute('data-id') === this.random.toString()) {
         if (this.prevElement) this.prevElement.removeChild(this.goblin);
         else this.goblin.classList.remove('hide');
         element.appendChild(this.goblin);
@@ -12,6 +16,14 @@ export default class App {
         return;
       }
     }
+  }
+
+  getRandom() {
+    let randomize = this.calculateRandom();
+    while (randomize === this.random) {
+      randomize = this.calculateRandom();
+    }
+    return randomize;
   }
 
   start() {
